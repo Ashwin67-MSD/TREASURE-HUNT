4850a15641b1bff9903bcb5ce57a92d7084984af
Ashwin67-MSD <73777149+Ashwin67-MSD@users.noreply.github.com>
2021-05-18 10:44:29 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "THOR";
+const accessCode2 = "TONYSTARK";
+const accessCode3 = "THANOS";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("O T R H", 100,50)
+    fill("lightblue")
+    text("Hint: GOD OF THUNDER !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    
+fill("white")
+textSize(15)
+text("O T Y N T S K R A", 700,150)
+fill("lightblue")
+text("Hint: IRON MAN", 700,170)
+
+fill("white")
+textSize(15)
+text(" N O T A H S", 100,250)
+fill("lightblue")
+text("Hint: MIGHTY VILLIAN", 100,270)
+}
+
